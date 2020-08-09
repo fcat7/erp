2afa6ff9ded503449de28954753e7ec1406148a9
fcat <18282065259@163.com>
2020-08-10 07:27:43 +0800

    Change Admin.java By erp

--- Admin.java
+++ Admin.java
@@ -1,6 +1,5 @@
-import java.util.List ;
+package vip.fcat.hr.vo ;
 public class Admin implements java.io.Serializable {
-	private List<Emp> emps ;
 	private String aid ;
-	private List<Action> allActions ;
+	private String password ;
 }
