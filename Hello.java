fc61d2013bc64b159934155e17b72bf95229525a
fcat <18282065259@163.com>
2020-08-09 16:22:32 +0800

    Test Merge

--- Hello.java
+++ Hello.java
@@ -2,6 +2,8 @@ import java.util.* ;
 public class Hello {
 	public static void main(String [] args) {
 		System.out.println("世界，你好！") ;
+		System.out.println("世界，你好！") ;
+		System.out.println("世界，你好！") ;
 		Admin admin = new Admin() ;
 		System.out.println("临近中午适合学习，因为饿了！") ;
 		Date date = new Date() ;
