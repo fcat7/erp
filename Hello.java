3436f7b8c5587c7c91a365eb7c8990238daf90e1
fcat <18282065259@163.com>
2020-08-09 15:06:41 +0800

    Change Hello.java And Add Action.java in dev Branch

--- Hello.java
+++ Hello.java
@@ -1,9 +1,7 @@
+import java.util.* ;
 public class Hello {
 	public static void main(String [] args) {
-		// System.out.println("Hello GIT !") ;
-		System.out.println("认真听课的孩子才是好孩子！") ;
-		System.out.println("不认真听课的孩子去后面站着！") ;
-		System.out.println("世界，你好！") ;
-		System.out.println(new java.util.Date()) ;
+		System.out.println("临近中午适合学习，因为饿了！") ;
+		Date date = new Date() ;
 	}
 }
