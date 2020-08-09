9f35eeb29a011572ee08e8e6c8500db1c95c8b73
fcat <18282065259@163.com>
2020-08-09 16:13:08 +0800

    Test Merge

--- Hello.java
+++ Hello.java
@@ -1,8 +1,6 @@
 import java.util.* ;
 public class Hello {
 	public static void main(String [] args) {
-		System.out.println("认真听课的孩子才是好孩子！") ;
-		System.out.println("不认真听课的孩子去后面站着！") ;
 		System.out.println("世界，你好！") ;
 		Admin admin = new Admin() ;
 		System.out.println("临近中午适合学习，因为饿了！") ;
